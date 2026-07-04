72eefe8263e1dfbed55a3fce856498b2c89c4be6
arya-1808 <shindearya0001@gmail.com>
2026-07-04 13:07:28 +0530

    add database_connection file

---
+++ db.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="arya2008",
+        database="hospital_management"
+    )
+print("Database connection established successfully.")
