2a255bc7aedb00936f84c144ad11ecff6ad7cd7a
arya-1808 <shindearya0001@gmail.com>
2026-07-12 14:00:40 +0530

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -17,7 +17,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS patient (
                patient_id INT AUTO_INCREMENT PRIMARY KEY, 
                name VARCHAR(255) NOT NULL, 
                age INT NOT NULL,
-               gender VARCHAR(10),
+               gender VARCHAR(10)
                
                )''')
 # print("Patient table created successfully.")
